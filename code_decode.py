d2b939b6b6af9e5867e1c4e7932b7825fc624381
Kritika Rohila <rohilakritika@gmail.com>
2024-08-08 14:25:45 +0530

    New Code File

---
+++ code_decode.py
@@ -0,0 +1,90 @@
+"""
+Write a python program to translate a message into secret code language. Use the rules below to translate normal English
+into secret code language
+
+Coding:
+if the word contains atleast 3 characters, remove the first letter and append it at the end now append three random
+characters at the starting and the end else: simply reverse the string
+
+Decoding:
+if the word contains less than 3 characters, reverse it else: remove 3 random characters from start and end. Now remove
+the last letter and append it to the beginning
+
+Your program should ask whether you want to code or decode
+"""
+import random
+import string
+
+
+# for coding the msg
+user = input("You want to code the message or decode the message (C/D) --> ").upper()
+
+if user == 'C' or user == 'D':
+    a = input("Enter your message --> ")
+    print(a)
+    b = list(a)
+    print("In list --> ", b)
+
+    # for coding the message
+    if user[0] == 'C':
+        # checking the length of input string if it is having more than 3 words then append the first
+        # letter to the end of the word
+        if len(b) > 3:
+            first_letter = b[0]
+            b.remove(first_letter)
+            print(b)
+            b.append(first_letter)
+            print("Code first to last --> ", b)
+            # 3 random characters for adding to the front and ending of the word
+            # adding in the front of the word
+            character = string.ascii_letters
+            # list comprehensions
+            front = (random.choice(character) for i in range(3))
+            front = list(front)
+            front.extend(b)
+            b = front
+            print(f"First added {b}")
+
+            # adding at the ending of the word
+            end = (random.choice(character) for i in range(3))
+            end = list(end)
+            b.extend(end)
+            print(f"end added {b}")
+
+            # changing the list to string
+            b = ''.join(i for i in b)
+            print("Coded Message --> ", b)
+            # LSdantoMiPG
+        # for less than 3 letters word --> we will reverse that words
+        else:
+            b.reverse()
+            b = ''.join(i for i in b)
+            print("Code --> ", b)
+
+    # for decoding the message
+    else:
+        if len(b) < 3:
+            b.reverse()
+            b = ''.join(i for i in b)
+            print("Message after decoding --> ", b)
+        else:
+            # removing the first 3 letters
+            b = b[3:]
+            print(b)
+
+            # removing the last 3 letters
+            b = b[:-3]
+            print(b)
+
+            # removing last letter ad add to first
+            last = b[-1]
+            del b[-1]
+            print(b)
+            b.insert(0,last)
+            print(b)
+
+            b = ''.join(i for i in b)
+            print("Message after Decoding finally --> ", b)
+else:
+    raise ValueError("Enter only C or D!")
+
